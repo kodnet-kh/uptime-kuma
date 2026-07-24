e099da8477f7de99d728e6bf8579e20e68c54fed
chhuny <amchhuny@gmail.com>
2026-07-24 10:16:05 +0700

    mod: update server down message

--- server/notification-providers/smtp.js
+++ server/notification-providers/smtp.js
@@ -104,7 +104,8 @@ class SMTP extends NotificationProvider {
         
         if (heartbeatJSON !== null) {
             if (heartbeatJSON["status"] === DOWN) {
-                bodyTextContent = `お客様各位\n現在、サーバーにて障害が発生しております。\n復旧対応として、約30分後にサーバーの再起動を実施予定です。\n再起動を控える必要がある場合は、お手数ですがご連絡ください。\nご不便をおかけいたしますが、何卒よろしくお願いいたします。`;
+                // bodyTextContent = `お客様各位\n現在、サーバーにて障害が発生しております。\n復旧対応として、約30分後にサーバーの再起動を実施予定です。\n再起動を控える必要がある場合は、お手数ですがご連絡ください。\nご不便をおかけいたしますが、何卒よろしくお願いいたします。`;
+                bodyTextContent = `お客様各位\n現在、貴社サーバーにて障害が発生しております。\nこれよりKODインフラチームにて速やかに調査を実施いたします。\nサーバーの再起動が必要な場合はコメントにてご連絡差し上げますので、実施時刻のご検討をお願いいたします。\n※なお、障害が自動的に解決されていた場合はその旨ご報告申し上げます。\nお忙しいところ恐縮ではございますが、何卒よろしくお願いいたします。`;
                 // send mail with defined transport object
                 await transporter.sendMail({
                     from: notification.smtpFrom,
